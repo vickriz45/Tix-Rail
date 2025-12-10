a76ba26910f68c7e4b58ae117c63e0a270bd9257
AlvinaNLA03 <alvinanurlaila31@gmail.com>
2025-12-10 21:24:44 +0700

    membuat xml profile dan item profile setting

--- app/src/main/java/id/ac/pnm/tix_rail/TiketSayaFragment.kt
+++ app/src/main/java/id/ac/pnm/tix_rail/TiketSayaFragment.kt
@@ -10,8 +10,7 @@ import androidx.fragment.app.Fragment
 class TiketSayaFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_tiket_saya, container, false)
     }
 
